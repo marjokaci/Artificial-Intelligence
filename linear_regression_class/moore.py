a255c3912cc4ec1261b8068361ef44954e6b34b0
marjokaci <38202934+marjokaci@users.noreply.github.com>
2019-02-07 12:52:15 +0100

    Update moore.py

--- linear_regression_class/moore.py
+++ linear_regression_class/moore.py
@@ -1,9 +1,4 @@
-# shows how linear regression analysis can be applied to moore's law
-#
-# notes for this course can be found at:
-# https://deeplearningcourses.com/c/data-science-linear-regression-in-python
-# https://www.udemy.com/data-science-linear-regression-in-python
-# transistor count from: https://en.wikipedia.org/wiki/Transistor_count
+
 
 from __future__ import print_function, division
 from builtins import range
