9b951d7053ceafcb91b42aa368af91f6322d9e0e
marjokaci <38202934+marjokaci@users.noreply.github.com>
2019-02-07 12:50:34 +0100

    Update l2_regularization.py

--- linear_regression_class/l2_regularization.py
+++ linear_regression_class/l2_regularization.py
@@ -1,8 +1,3 @@
-# demonstration of L2 regularization
-#
-# notes for this course can be found at:
-# https://deeplearningcourses.com/c/data-science-linear-regression-in-python
-# https://www.udemy.com/data-science-linear-regression-in-python
 
 from __future__ import print_function, division
 from builtins import range
