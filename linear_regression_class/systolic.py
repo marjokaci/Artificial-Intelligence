87bd1543449f94a6d51f139ffa47f15bebccda57
marjokaci <38202934+marjokaci@users.noreply.github.com>
2019-02-07 12:53:08 +0100

    Update systolic.py

--- linear_regression_class/systolic.py
+++ linear_regression_class/systolic.py
@@ -1,7 +1,3 @@
-# https://deeplearningcourses.com/c/data-science-linear-regression-in-python
-# need to sudo pip install xlrd to use pd.read_excel
-# data is from:
-# http://college.cengage.com/mathematics/brase/understandable_statistics/7e/students/datasets/mlr/frames/mlr02.html
 
 # The data (X1, X2, X3) are for each patient.
 # X1 = systolic blood pressure
